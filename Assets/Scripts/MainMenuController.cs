3c511bad81a4d8a705cfa2a0226f9d7c06a04f4c
Quinlan <qbhoang@ucsc.edu>
2024-12-05 00:06:22 -0800

    UI

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public CanvasGroup SettingsPanel;
+    public CanvasGroup CreditsPanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SceneManager.GetActiveScene().buildIndex + 1");
+    }
+
+    public void Settings()
+    {
+        SettingsPanel.alpha = 1;
+        SettingsPanel.blocksRaycasts = true;
+    }
+
+    public void Credits()
+    {
+        CreditsPanel.alpha = 2;
+        CreditsPanel.blocksRaycasts = true;
+    }
+
+    public void Back()
+    {
+        SettingsPanel.alpha = 0;
+        SettingsPanel.blocksRaycasts = false;
+        CreditsPanel.alpha = 0;
+        CreditsPanel.blocksRaycasts = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
+
